22ecbea32dec8ac66e93463af0792c6bfccbc15c
bhagat2296 <103653090+bhagat2296@users.noreply.github.com>
2022-07-31 21:24:41 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,237 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*for(int i=0;i<=4;i++)          //question 1 by for loop
+    {printf("My SirG\n");}*/
+
+
+    /*int i=0;
+    while(i<=4)                      //by while loop
+    {
+        printf("My SirG \n");
+        i++;
+
+    }*/
+
+    /*int i=0;
+    do                               //by do while loop
+    {
+        printf("My Sirg \n");
+        i++;
+    }while(i<=4);*/
+
+
+    /*for(int i=1;i<=10;i++)          //Qustion 2 by for loop
+        printf("%d\n",i);*/
+
+
+        /*int i=1;                   //By while loop
+        while(i<=10)
+    {
+        printf("%d\n",i);
+        i++;
+    }*/
+
+
+    /*int i=1;                    //By do while loop
+    do
+    {
+        printf("%d\n",i);
+        i++;
+    }while(i<=10);*/
+
+
+
+    /*for(int i=10;i>=1;i--)          //Qustion 3 by for loop
+        printf("%d\n",i);*/
+
+
+        /*int i=10;                   //By while loop
+        while(i>=1)
+    {
+        printf("%d\n",i);
+        i--;
+    }*/
+
+
+    /*int i=10;                      //By do while loop
+    do
+    {
+        printf("%d\n",i);
+        i--;
+    }while(i>=1);*/
+
+
+
+
+    /*for(int i=1;i<=20;)          //Qustion 4 by for loop
+        {
+            printf("%d\n",i);
+            i=i+2;
+        }*/
+
+
+       /* int i=1;                   //By while loop
+        while(i<=20)
+    {
+        printf("%d\n",i);
+        i+=2;;
+    }*/
+
+
+     /*int i=1;                    //By do while loop
+    do
+    {
+        printf("%d\n",i);
+        i+=2;
+    }while(i<=20);*/
+
+
+
+
+    /*for(int i=20;i>=1;i--)          //Qustion 5 by for loop
+        {
+            if(i%2)
+
+            {
+                printf("%d\n",i);
+            }
+
+        }*/
+
+
+       /*int i=20;                      //By while loop
+        while(i>=1)
+    {
+        if(i%2)
+        {
+            printf("%d\n",i);
+        }
+        i--;
+    }*/
+
+
+     /*int i=20;                      //By do while loop
+    do
+    {
+        if(i%2)
+        {
+        printf("%d\n",i);
+
+        }
+        i--;
+    }while(i>=1);*/
+
+
+
+
+   /* for(int i=1;i<=20;i++)          //Qustion 6 by for loop
+        {
+            if(i%2==0)
+            {
+
+            printf("%d\n",i);
+            }
+
+        }*/
+
+
+        /*int i=1;                   //By while loop
+        while(i<=20)
+    {
+        if(i%2==0)
+        printf("%d\n",i);
+        i++;
+    }*/
+
+
+     /*int i=1;                    //By do while loop
+    do
+    {
+        if(i%2==0)
+        printf("%d\n",i);
+        i++;
+    }while(i<=20);*/
+
+
+      /*for(int i=20;i>=1;i--)          //Qustion 7 by for loop
+        {
+            if(i%2==0)
+            printf("%d\n",i);
+
+
+        }*/
+
+
+        /*int i=20;                   //By while loop
+        while(i>=1)
+    {
+        if(i%2==0)
+        printf("%d\n",i);
+        i--;
+    }*/
+
+
+     /*int i=20;                    //By do while loop
+    do
+    {
+        if(i%2==0)
+        printf("%d\n",i);
+        i--;
+    }while(i>=1);*/
+
+
+    /*for(int i=1;i<=10;i++)          //Qustion 8 by for loop
+        printf("%d\n",i*i);*/
+
+
+        /*int i=1;                   //By while loop
+        while(i<=10)
+    {
+        printf("%d\n",i*i);
+        i++;
+    }*/
+
+
+    /*int i=1;                    //By do while loop
+    do
+    {
+        printf("%d\n",i*i);
+        i++;
+    }while(i<=10);*/
+
+
+
+    /*for(int i=1;i<=10;i++)          //Qustion 9 by for loop
+        printf("%d\n",i*i*i);*/
+
+
+        /*int i=1;                   //By while loop
+        while(i<=10)
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }*/
+
+
+    /*int i=1;                    //By do while loop
+    do
+    {
+        printf("%d\n",i*i*i);
+        i++;
+    }while(i<=10);*/
+
+
+   /* for(int i=1;i<=10;i++)       //Question 10 By for loop
+        printf("%d\n",5*i);*/
+
+
+
+
+
+
+    return 0;
+}
+
